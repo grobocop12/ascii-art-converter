b7ee1d85880ce4f30e46312558c844fc32cc5780
grobocop12 <chasnik97876@gmail.com>
2018-12-26 14:02:47 +0100

    file explorer activity

--- ascii-art-converter/app/src/main/java/com/example/grobocop/ascii_art_converter/MainActivity.java
+++ ascii-art-converter/app/src/main/java/com/example/grobocop/ascii_art_converter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.grobocop.ascii_art_converter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickTakePhoto(View v) {
+
+    }
+
+    public void onClickExploreFiles(View v) {
+        Intent intent = new Intent(this, ExploreFilesActivity.class);
+        startActivity(intent);
     }
 }
