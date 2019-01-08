ef2064071664aa6247baf8167a60373eb0f0ea9d
grobocop12 <chasnik97876@gmail.com>
2019-01-08 23:28:51 +0100

    remove redundant activity

--- ascii-art-converter/app/src/main/java/com/example/grobocop/ascii_art_converter/MainActivity.java
+++ ascii-art-converter/app/src/main/java/com/example/grobocop/ascii_art_converter/MainActivity.java
@@ -1,24 +1,83 @@
 package com.example.grobocop.ascii_art_converter;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import java.util.regex.Pattern;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_IMAGE_PATH = "com.example.grobocop.ascii_art_converter.EXTRA_IMAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1001);
+        }
+
+        Button exploreButton = (Button) findViewById(R.id.explore_files_button);
+        exploreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialFilePicker()
+                        .withActivity(MainActivity.this)
+                        .withRequestCode(1)
+                        .withFilter(Pattern.compile(".*\\.jpg")) // Filtering files and directories by file name using regexp
+                        .withFilterDirectories(false) // Set directories filterable (false by default)
+                        .withHiddenFiles(true) // Show hidden files and folders
+                        .start();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            // Do anything with file
+            Intent intent = new Intent(this, DisplayImageActivity.class);
+            intent.putExtra(EXTRA_IMAGE_PATH, filePath);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1001: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
     }
 
     public void onClickTakePhoto(View v) {
 
     }
 
-    public void onClickExploreFiles(View v) {
-        Intent intent = new Intent(this, ExploreFilesActivity.class);
-        startActivity(intent);
-    }
 }
