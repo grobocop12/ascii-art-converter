6a64567b0ce29eff9d7bbc8f74c1438222040d1c
grobocop <chasnik97876@gmail.com>
2019-01-10 01:03:54 +0100

    stable converter

--- ascii-art-converter/app/src/main/java/com/example/grobocop/ascii_art_converter/ConvertImageActivity.java
+++ ascii-art-converter/app/src/main/java/com/example/grobocop/ascii_art_converter/ConvertImageActivity.java
@@ -2,18 +2,26 @@ package com.example.grobocop.ascii_art_converter;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spannable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+//import com.bachors.img2ascii.Img2Ascii;
 
 import com.bachors.img2ascii.Img2Ascii;
 
-import org.w3c.dom.Text;
-
 public class ConvertImageActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView asciiTextView;
+
+    private static final String TAG = "Convert";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +29,31 @@ public class ConvertImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_convert_image);
 
         String imagePath = getIntent().getStringExtra(MainActivity.EXTRA_IMAGE_PATH);
+
         Bitmap image = BitmapFactory.decodeFile(imagePath);
 
-        textView = (TextView) findViewById(R.id.asciiText);
+        asciiTextView = (TextView) findViewById(R.id.asciiart);
 
-        int quality = 1; // 1 - 3
+       // String yourAsciiArtData = ""; // from assets or network or...
 
-// convert image(bitmap) to ascii(string)
-        new Img2Ascii()
+       /* AsciiArtView asciiArtView = (AsciiArtView) findViewById(R.id.asciiart);
+        asciiArtView.setAsciiArt(yourAsciiArtData);
+        asciiArtView.setTypeface(Typeface.create( "Sans-serif", Typeface.NORMAL));
+*/
+
+       new Img2Ascii()
                 .bitmap(image)
-                .quality(3) // 1 - 5
-                //.color(true)
+                .quality(2) // 1 - 5
+                .color(true)
                 .convert(new Img2Ascii.Listener() {
                     @Override
                     public void onProgress(int percentage) {
-                        textView.setText(String.valueOf(percentage) + " %");
+                        asciiTextView.setText(String.valueOf(percentage) + " %");
+                        Log.d(TAG, String.valueOf(percentage) + " %");
                     }
                     @Override
                     public void onResponse(Spannable text) {
-                        textView.setText(text);
+                        asciiTextView.setText(text);
                     }
                 });
 
