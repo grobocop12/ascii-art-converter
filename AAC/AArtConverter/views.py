969abd39af0933e4dd19b1aba38cc060fc123344
grobocop12 <chasnik97876@gmail.com>
2019-01-23 14:13:22 +0100

    set image size

--- AAC/AArtConverter/views.py
+++ AAC/AArtConverter/views.py
@@ -17,23 +17,22 @@ from django.views.generic import TemplateView
 
 @csrf_exempt
 def index(request):
+    screen = aalib.AsciiScreen(width = 160, height = 144)
     if request.method == 'POST':
-        for key in request.FILES:
-            print(key)
         uploaded_file = request.FILES['uploaded_file']
-        print(type(uploaded_file))
-        image = PIL.Image.open(uploaded_file,'r').convert('L')
+        image = PIL.Image.open(uploaded_file,'r').convert('L').resize(screen.virtual_size)
     
         #image =  PIL.Image.open('lena.jpg','r').convert('L')
     
-    size = image.size    
-    print(size)
-    screen = aalib.AsciiScreen(width = size[0], height=size[1])
+    #screen = aalib.AsciiScreen()
     
     #image =  PIL.Image.open('lena.jpg','r').convert('L').resize(screen.virtual_size)
     screen.put_image((0,0),image)
+    print(screen.virtual_size)
     
-    img = PIL.Image.new('RGB',(size[0],size[1]), color=(0,0,0))
+    #img = PIL.Image.new('RGB',(int(screen.virtual_size[0]*3),screen.virtual_size[1]*4), color=(0,0,0))
+    
+    img = PIL.Image.new('RGB',(900,2150), color=(0,0,0))
     d = PIL.ImageDraw.Draw(img)
     d.text((0,0), screen.render(), fill=(255, 255, 255),align='center' )
     name = str(uuid.uuid4())+'.png'
@@ -43,7 +42,5 @@ def index(request):
     data = {
         'url':('/static/'+name)
         }
-    
     #return HttpResponse(image_data, content_type="image/png")
     return JsonResponse(data)
-
